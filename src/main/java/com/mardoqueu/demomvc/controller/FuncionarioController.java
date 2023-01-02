4ec690e8e1e1f38999aa6605fda3f901c3b14ded
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-02 13:16:02 -0300

    adicionando rotas e testando links do menu

--- src/main/java/com/mardoqueu/demomvc/controller/FuncionarioController.java
+++ src/main/java/com/mardoqueu/demomvc/controller/FuncionarioController.java
@@ -1,2 +1,21 @@
-package com.mardoqueu.demomvc.controller;public class FuncionarioController {
+package com.mardoqueu.demomvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("funcionarios")
+public class FuncionarioController {
+
+    @GetMapping("/cadastrar")
+    public String cadastrar(){
+        return "/funcionario/cadastro";
+    }
+
+    @GetMapping("/listar")
+    public String listar(){
+        return "/funcionario/lista";
+    }
+
 }
