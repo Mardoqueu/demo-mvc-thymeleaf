8ac762b75648571907d432f45da1081e3be9cc05
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-02 13:15:31 -0300

    adicionando rotas e testando links

--- src/main/java/com/mardoqueu/demomvc/controller/DepartamentoController.java
+++ src/main/java/com/mardoqueu/demomvc/controller/DepartamentoController.java
@@ -1,2 +1,20 @@
-package com.mardoqueu.demomvc.controller;public class DepartamentoController {
+package com.mardoqueu.demomvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/departamentos")
+public class DepartamentoController {
+
+    @GetMapping("/cadastrar")
+    public String cadastrar(){
+        return "/departamento/cadastro";
+    }
+
+    @GetMapping("/listar")
+    public String listar(){
+        return "/departamento/lista";
+    }
 }
