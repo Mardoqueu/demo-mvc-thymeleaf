8d793a04565c6fa386068cb71eecca5d32ff2292
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-02 12:06:14 -0300

    getting started

---
+++ src/main/java/com/mardoqueu/demomvc/DemoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.mardoqueu.demomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mardoqueu/demomvc/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mardoqueu.demomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
