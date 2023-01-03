c6a2a0b53f27096f95bec2ae1701b7d72f5b767a
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-03 13:40:40 -0300

    mapeamento da entidade Cargo

---
+++ src/main/java/com/mardoqueu/demomvc/domain/Cargo.java
@@ -0,0 +1,31 @@
+package com.mardoqueu.demomvc.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CARGOS")
+public class Cargo extends AbstractEntity<Long>{
+
+    @Column(name = "nome", nullable = false, unique = true, length = 60)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "id_departamento")
+    private Departamento departamento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+}
