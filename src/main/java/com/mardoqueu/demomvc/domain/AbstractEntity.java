5de141b80dee38700369d2ee0a700f5df6611a3f
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-03 13:33:23 -0300

    criando super classe para entidades

---
+++ src/main/java/com/mardoqueu/demomvc/domain/AbstractEntity.java
@@ -0,0 +1,40 @@
+package com.mardoqueu.demomvc.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity<ID extends Serializable> implements Serializable{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractEntity)) return false;
+        AbstractEntity<?> that = (AbstractEntity<?>) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id ;
+    }
+}
