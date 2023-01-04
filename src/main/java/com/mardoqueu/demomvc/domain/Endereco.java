f2b39cde172e255d7ab933d8f5925d7dc220bd3d
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-04 19:24:30 -0300

    criacao da tabela endereco com atributos, getters e setters

---
+++ src/main/java/com/mardoqueu/demomvc/domain/Endereco.java
@@ -0,0 +1,88 @@
+package com.mardoqueu.demomvc.domain;
+
+import com.mardoqueu.demomvc.domain.enums.UF;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ENDERECOS")
+public class Endereco extends  AbstractEntity<Long>{
+
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false, length = 2)
+    @Enumerated(EnumType.STRING)
+    private UF uf;
+
+    @Column(nullable = false, length = 9)
+    private String cep;
+
+    @Column(nullable = false, length = 5)
+    private Integer numero;
+
+    @Column(nullable = false, length = 2)
+    private String complemento;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public UF getUf() {
+        return uf;
+    }
+
+    public void setUf(UF uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
