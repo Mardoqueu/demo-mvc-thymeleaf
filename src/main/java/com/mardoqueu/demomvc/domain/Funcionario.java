70751542921901dc75cbb2b6184c904e8db4a592
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-04 19:37:48 -0300

    criacao da classe funcionarios com atributos, getters e setters

---
+++ src/main/java/com/mardoqueu/demomvc/domain/Funcionario.java
@@ -0,0 +1,78 @@
+package com.mardoqueu.demomvc.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "FUNCIONARIOS")
+public class Funcionario extends AbstractEntity<Long>{
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "DECIMAL(7, 2) DEFAULT 0.00")
+    private BigDecimal salario;
+
+    @Column(name = "data_entrada", nullable = false, columnDefinition = "DATE")
+    private LocalDate dataEntrada;
+
+    @Column(name = "data_saida", columnDefinition = "DATE")
+    private LocalDate dataSaida;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id_fk")
+    private Endereco endereco;
+
+    @ManyToOne
+    @JoinColumn(name = "cargo_id_fk")
+    private Cargo cargo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public LocalDate getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(LocalDate dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public LocalDate getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(LocalDate dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+}
